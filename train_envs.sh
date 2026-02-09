8b6d35f40e180f3f0007f9761fe6478cfe2987bc
Carlos Jiménez Farfán <cjimenezf17@gmail.com>
2026-02-09 11:19:06 +0100

    update training bash script

--- train_envs.sh
+++ train_envs.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Door environment
+python3 scripts/train.py --env Door --algorithm PPO --total_timesteps 500_000 
+python3 scripts/train.py --env Door --algorithm SAC --total_timesteps 500_000 
+
+# NutAssemblySquare environment
+python3 scripts/train.py --env NutAssemblySquare --algorithm PPO --total_timesteps 500_000 
+python3 scripts/train.py --env NutAssemblySquare --algorithm SAC --total_timesteps 500_000
 
 # WIPE environment
 python3 scripts/train.py --env Wipe --algorithm PPO --total_timesteps 1_000_000 
 python3 scripts/train.py --env Wipe --algorithm SAC --total_timesteps 1_000_000 
-
 
 
 # NutAssemblyRound environment
