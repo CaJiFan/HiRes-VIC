1ae923cc1e35d83fc7361ac8215695e474ead0e6
CaJiFan <cjimenezf17@gmail.com>
2026-03-02 18:57:26 +0100

    update evaluate script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -9,6 +9,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 
 import os 
 import sys
+import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from hires_vic.envs.gymnasium_wrapper import RobosuiteGymnasiumWrapper, RobosuitePhysicsWrapper
 from robosuite import load_composite_controller_config
@@ -60,12 +61,17 @@ def make_env(run_name, env_name, rank=0, seed=0):
     env = Monitor(env)
     return env
 
+def render_delay_callback(_locals, _globals):
+    time.sleep(0.01)
+    return True
+
+
 def main():
     args = parse_args()
     device = "cuda" if torch.cuda.is_available() else "cpu"
     env_name = args.env
     
-    model_path = f"./outputs/models/{args.algorithm}_{env_name.lower()}_{device}_{args.run_name}_final"
+    model_path = f"./outputs/models/{args.algorithm}_{env_name.lower()}_{args.run_name}_final"
     
     # Create the environment and wrap it in a VecEnv (required for evaluate_policy)
     env = make_env(args.run_name, env_name)
@@ -83,14 +89,15 @@ def main():
     
     print(f"Running formal evaluation over {args.episodes} episodes...")
     
-    # ---> THE MAGIC FUNCTION <---
+
     # This replaces your entire while loop!
     mean_reward, std_reward = evaluate_policy(
         model, 
         vec_env, 
         n_eval_episodes=args.episodes, 
         deterministic=True, 
-        render=True # Automatically calls env.render() for you
+        render=True,
+        callback=render_delay_callback
     )
 
     # Note: evaluate_policy automatically prints progress if you pass return_episode_rewards=False
